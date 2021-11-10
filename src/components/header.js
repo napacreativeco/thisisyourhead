a67bfebf203e4a8145e2dc00cfdeecf85abe7535
James Rogers <hello@napacreativeco.com>
2021-11-10 15:51:24 -0800

    fixing header

--- src/components/header.js
+++ src/components/header.js
@@ -10,12 +10,6 @@ const Header = () => (
       <div className="cell contact">
         <a href="mailto:thisisyourhead@gmail.com">CONTACT<br />FOR CUSTOM DESIGN</a>
       </div>
-      <div className="cell shop" style={{ 'cursor': 'not-allowed' }}>
-        SHOP ONLINE<br />COMING SOON
-      </div>
-      <div className="cell cart" style={{ 'cursor': 'not-allowed' }}>
-        CART<br />0 ITEMS
-      </div>
     </div>
 
     <p style={{ paddingLeft: '10px' }}>Multidisciplinary artist based in Napa, Calfornia</p>
